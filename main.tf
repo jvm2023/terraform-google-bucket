576640b614d99d0d7de6e04c6e8e8b2e731e31eb
jvm2023 <125207795+jvm2023@users.noreply.github.com>
2024-06-17 16:30:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ resource "google_storage_bucket" "testbucket" {
     name = var.name
     location = var.location
     force_destroy = var.force_destroy
+    uniform_bucket_level_access = var.uniform_bucket_level_access
   
 
 
